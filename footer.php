b9e81787beffdffb3d3aad150f55f025f9a67bd0
totte <totte@tott.es>
2013-05-01 23:19:41 +0200

    Split mockup.html content into header.php and footer.php

--- footer.php
+++ footer.php
@@ -7,20 +7,18 @@
  * @package Laponia
  */
 ?>
-
-	</div><!-- #main -->
-
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<?php do_action( 'laponia_credits' ); ?>
+	    </div>
+	    <hr>
+            <footer>
+                <p>Copyright 2013 H W "totte" Tovetjärn</p>
+                <?php do_action( 'laponia_credits' ); ?>
 			<a href="http://wordpress.org/" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', 'laponia' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', 'laponia' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
 			<?php printf( __( 'Theme: %1$s by %2$s.', 'laponia' ), 'Laponia', '<a href="http://tott.es" rel="designer">totte</a>' ); ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
-
-<?php wp_footer(); ?>
-
-</body>
+            </footer>
+        </div> <!-- /.container-narrow -->
+        <script src="js/jquery-1.9.1.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+	<?php wp_footer(); ?>
+    </body>
 </html>
--- header.php
+++ header.php
@@ -6,36 +6,62 @@
  *
  * @package Laponia
  */
-?><!DOCTYPE html>
+?>
+<!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-<head>
-<meta charset="<?php bloginfo( 'charset' ); ?>" />
-<meta name="viewport" content="width=device-width" />
-<title><?php wp_title( '|', true, 'right' ); ?></title>
-<link rel="profile" href="http://gmpg.org/xfn/11" />
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-<!--[if lt IE 9]>
-<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
-<![endif]-->
-
-<?php wp_head(); ?>
-</head>
-
-<body <?php body_class(); ?>>
-<div id="page" class="hfeed site">
+    <head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<title><?php wp_title( '|', true, 'right' ); ?></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="assorted ramblings and creations">
+        <meta name="author" content="totte">
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,600,700' rel='stylesheet' type='text/css'>
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/bootstrap-responsive.min.css" rel="stylesheet">
+        <link href="css/style.css" rel="stylesheet">
+        <link rel="profile" href="http://gmpg.org/xfn/11" />
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+        <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+        <!--[if lt IE 9]>
+            <script src="bootstrap/js/html5shiv.js"></script>
+        <![endif]-->
+        <?php wp_head(); ?>
+    </head>
+    <body <?php body_class(); ?>>
 	<?php do_action( 'before' ); ?>
-	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
-		</div>
-
-		<nav id="site-navigation" class="navigation-main" role="navigation">
-			<h1 class="menu-toggle"><?php _e( 'Menu', 'laponia' ); ?></h1>
-			<div class="screen-reader-text skip-link"><a href="#content" title="<?php esc_attr_e( 'Skip to content', 'laponia' ); ?>"><?php _e( 'Skip to content', 'laponia' ); ?></a></div>
-
-			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
-
-	<div id="main" class="site-main">
+        <div class="container-narrow">
+            <div class='navbar'>
+                <div class="navbar-inner">
+                    <div class="container">
+                        <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </a>
+                        <a class="brand" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+                        <div class="nav-collapse collapse">
+                            <ul class="nav pull-right">
+                                <li class="active"><a href="#">blog</a></li>
+                                <li><a href="#">drawings</a></li>
+                                <li><a href="#">photographs</a></li>
+                                <li><a href="#">code</a></li>
+                                <li><a href="#">books</a></li>
+                                <li><a href="#">links</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="jumbotron">
+                <p class="lead"><?php bloginfo( 'description' ); ?></p>
+            </div>
+            <hr>
+            <div class="row-fluid">
+<?php
+/**
+* <nav id="site-navigation" class="navigation-main" role="navigation">
+* <h1 class="menu-toggle"><?php _e( 'Menu', 'laponia' ); ?></h1>
+* <div class="screen-reader-text skip-link"><a href="#content" title="<?php esc_attr_e( 'Skip to content', 'laponia' ); ?>"><?php _e( 'Skip to content', 'laponia' ); ?></a></div>
+* <?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
+* </nav><!-- #site-navigation -->
+*/
